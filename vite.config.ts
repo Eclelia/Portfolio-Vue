addc56f3fbed8818a73b7403f53a5f2db0a8965a
Célia Dennetiere <123080314+Eclelia@users.noreply.github.com>
2024-08-23 15:12:32 +0200

    fix(deploy): error in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
+  base: "https://eclelia.github.io/Portfolio-Vue/",
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
